32767af5b894ccdbd42da40142453f546153e0e3
khaledelhady44 <khaledelhady44@gmail.com>
2024-10-05 09:49:44 +0300

    hope to be final

--- API/api.py
+++ API/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException, Response
 from fastapi.responses import JSONResponse, FileResponse
 from pydantic import BaseModel
+import numpy as np
 import json
 import os
 import time
@@ -19,6 +20,20 @@ app = FastAPI()
 # os.makedirs(os.path.join(UPLOAD_DIR, PROCESSED_DIR), exist_ok=True)
 sta_lst = STALTA()
 
+def plot(st, arrival):
+    fig, ax = plt.subplots(1,1,figsize=(12,3))
+    ax.axvline(x = arrival, color='red', label='Trig. On')
+
+    tr_times = st[0].times()
+    tr_data = st[0].data    
+
+    # Plot seismogram
+    plt.plot(tr_times, tr_data)
+    random_name = f"plot_{np.random.randint(10000)}.png"
+    plt.savefig(f"../MoonQuake/analyzer/static/images/{random_name}") 
+    return "/images/{random_name}"
+
+
 
 @app.post("/get-stalta-startpoint")
 async def get_quake_startpoint(file: UploadFile = File(...)):
@@ -31,16 +46,8 @@ async def get_quake_startpoint(file: UploadFile = File(...)):
 
     st = read(SAVE_FILE_PATH+file.filename)
     arrival = sta_lst.get_start(st)
-    fig, ax = plt.subplots(1,1,figsize=(12,3))
-    ax.axvline(x = arrival, color='red', label='Trig. On')
-
-    tr_times = st[0].times()
-    tr_data = st[0].data    
-
-    # Plot seismogram
-    plt.plot([1, 2, 3, 4], [10, 20, 25, 30])
-    plt.savefig("MoonQuake/analyzer/static/images/plot.png") 
-    return JSONResponse({"startPoint": time})
+    
+    return JSONResponse({"startPoint": arrival, "path": plot(st, arrival)})
 
 
 @app.post("/get-vit-startpoint")
@@ -54,5 +61,7 @@ async def get_quake_startpoint(file: UploadFile = File(...)):
 
     vel, mode, st = from_ms2data(SAVE_FILE_PATH+file.filename)
     use_vit = VitInference(model_path="vit_seismic.pt",data_max_length=572427)
-    time = use_vit(vel=vel,mode=mode)
-    return JSONResponse({"startPoint": time})
+    arrival = use_vit(vel=vel,mode=mode)
+
+
+    return JSONResponse({"startPoint": arrival, "path": plot(st, arrival)})
